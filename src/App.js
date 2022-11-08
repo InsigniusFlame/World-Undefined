357995118b750cbfc27d32194bdc7c57385bf8b4
InsigniusFlame <insigniustwitch@gmail.com>
2022-11-08 11:16:41 +0530

    reinitialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>WORLD 1/0</h1>
+    </div>
+  );
+}
+
+export default App;
